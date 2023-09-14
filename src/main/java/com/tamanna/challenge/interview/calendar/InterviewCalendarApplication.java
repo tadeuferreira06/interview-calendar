c4a777a93496b7bf5dfcae9068d9016611aca1ec
Tadeu Ferreira <tadeu.ferreira@wit-software.com>
2023-09-14 18:24:32 +0100

    Initial project setup with Spring Initializr

---
+++ src/main/java/com/tamanna/challenge/interview/calendar/InterviewCalendarApplication.java
@@ -0,0 +1,13 @@
+package com.tamanna.challenge.interview.calendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewCalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewCalendarApplication.class, args);
+	}
+
+}
